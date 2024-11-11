828e401142ba5410cde453e59e0a3762510c3918
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2024-11-11 21:03:24 +0200

    Created NUnit Test project

---
+++ Betsson.OnlineWallets.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Betsson.OnlineWallets.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
