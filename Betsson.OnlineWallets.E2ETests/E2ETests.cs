8f9c42820af89c4c62c88e4e7da15d48eeae93e2
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2024-11-13 11:43:06 +0200

    Withdraw with Sufficient Balance properly updates balance E2E Test

--- Betsson.OnlineWallets.E2ETests/E2ETests.cs
+++ Betsson.OnlineWallets.E2ETests/E2ETests.cs
@@ -65,7 +65,7 @@ namespace Betsson.OnlineWallets.E2ETests
             var getBalance = JsonSerializer.Deserialize<BalanceResponse>(getBalanceResponse.Content, options);
             Console.WriteLine($"GetBalance Response JSON: {getBalanceResponse.Content}");
 
-            var startingBalance = getBalance?.Amount;
+            var startingBalance = (decimal)getBalance?.Amount;
 
             // Then create the request, execute it and wait for the response.
             var request = new RestRequest("/onlinewallet/deposit", Method.Post);
@@ -81,7 +81,7 @@ namespace Betsson.OnlineWallets.E2ETests
 
             Console.WriteLine($"Deposit Response JSON: {response.Content}");
 
-            // Then ensure the new balance is greater than or equal to the deposit amount.
+            // Then ensure the new balance is equal to startingBalance + depositAmount
             // Deserialize with case-insensitive options
             var depositBalanceResponse = JsonSerializer.Deserialize<BalanceResponse>(response.Content, options);
             Assert.That(depositBalanceResponse?.Amount, Is.EqualTo(depositAmount + startingBalance), $"Balance within deposit response should be equal to the deposited amount ({depositAmount}) + startingBalance ({startingBalance}).");
@@ -91,6 +91,53 @@ namespace Betsson.OnlineWallets.E2ETests
             // Deserialize with case-insensitive options
             getBalance = JsonSerializer.Deserialize<BalanceResponse>(getBalanceResponse.Content, options);
             Assert.That(getBalance?.Amount, Is.EqualTo(depositAmount + startingBalance), $"Balance from GetBalance after the deposit should be equal to the deposited amount ({depositAmount}) + starting balance ({startingBalance}).");
+        }
+
+        /// <summary>
+        /// Sends a POST request to withdraw funds, after checking balance, ensuring the amount to withdraw is sufficient.
+        /// Then checks the funds are properly updated, and balance is properly updated as well.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task WithdrawFunds_DecreasesBalance()
+        {
+            // First create a request, execute and wait for the response of GetBalance.
+            var getBalanceRequest = new RestRequest("/onlinewallet/balance", Method.Get);
+            var getBalanceResponse = await _client.ExecuteAsync(getBalanceRequest);
+
+            // Deserialize with case-insensitive options
+            var getBalance = JsonSerializer.Deserialize<BalanceResponse>(getBalanceResponse.Content, options);
+            Console.WriteLine($"GetBalance Response JSON: {getBalanceResponse.Content}");
+
+            var startingBalance = (decimal)(getBalance?.Amount);
+
+            // Then create the request, execute it and wait for the response.
+            var request = new RestRequest("/onlinewallet/withdraw", Method.Post);
+
+            // Ensure the amount to withdraw is less than the startingBalance (or equal to, in case of 0 balance).
+            var withdrawAmount = Math.Round(startingBalance / 2.0M, 1, MidpointRounding.ToZero);
+            request.AddJsonBody(new { amount = withdrawAmount });
+
+            var response = await _client.ExecuteAsync(request);
+
+            // Then ensure the response is successful, with non-empty contents.
+            Assert.That(response.IsSuccessful, Is.True, "Withdraw should be successful");
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(response.Content, Is.Not.Empty, "Response should not have empty contents");
+
+            Console.WriteLine($"Withdraw Response JSON: {response.Content}");
+
+            // Then ensure the new balance is equal to starting balance - withdraw amount
+            // Deserialize with case-insensitive options
+            var withdrawBalanceResponse = JsonSerializer.Deserialize<BalanceResponse>(response.Content, options);
+            Assert.That(withdrawBalanceResponse?.Amount, Is.EqualTo(startingBalance - withdrawAmount), $"Balance within withdraw response should be equal to the startingBalance ({startingBalance}) - withdraw amount ({withdrawAmount}).");
+
+            // Then ensure GetBalance properly returns the expected balance.
+            getBalanceResponse = await _client.ExecuteAsync(getBalanceRequest);
+            // Deserialize with case-insensitive options
+            getBalance = JsonSerializer.Deserialize<BalanceResponse>(getBalanceResponse.Content, options);
+            Assert.That(getBalance?.Amount, Is.EqualTo(startingBalance - withdrawAmount), $"Balance from GetBalance after the withdrawal should be equal to the starting balance ({startingBalance}) - the withdrawn amount ({withdrawAmount}).");
+
         }
     }
 }
